dae9c98ddaba3f7ad5ca623995cc7658f9c5d49b
= <martinezvallsmarc@gmail.com>
2024-06-19 09:49:35 +0200

    tests-corregits

--- list/models.py
+++ list/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from board.models import Board
 
 # Create your models here.
 class List(models.Model):
     id = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=100)
-    board_id = models.BigIntegerField()
+    board = models.ForeignKey(Board, on_delete=models.CASCADE,null=True)
     position = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     
