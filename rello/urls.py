f7cb77e23eff0c01f8e99bbdbd0f474974cc59e8
= <martinezvallsmarc@gmail.com>
2024-07-09 15:33:07 +0200

    admin-posat-de-nou

--- rello/urls.py
+++ rello/urls.py
@@ -19,7 +19,7 @@ from django.urls import include,path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-   # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('rello/api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('rello/api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
     path('rello/api/v1/', include('board.urls')),
